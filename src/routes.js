3e0944def1d333686e1e90a553b0b6110001fa7a
Antônio Evaldo <antonio.evaldo349@gmail.com>
2022-08-18 16:35:45 -0300

    removendo warning de rota

--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,7 @@ function AppRoutes() {
           <Route path="sobremim" element={<SobreMim />} />
         </Route>
         
-        <Route path="posts/:id" element={<Post />} />
+        <Route path="posts/:id/*" element={<Post />} />
         <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
