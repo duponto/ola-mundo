622d484a0f8a0c5d241d73a79a7f9ae0237718bd
Antônio Evaldo <antonio.evaldo349@gmail.com>
2022-08-18 10:18:40 -0300

    vídeo 4.2

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,11 +1,28 @@
+import './Post.css';
+
 import { useParams } from "react-router-dom"
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
 
 export default function Post() {
     const parametros = useParams();
 
-    console.log(parametros);
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id);
+    })
 
     return (
-        <h1>Post {parametros.id}</h1>
+        <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+            
+        </PostModelo>
     )
 }
